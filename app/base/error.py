3a4290ac31deed3fe62710ba7ea848d6b45dae74
er1cw00 <wadahana@qq.com>
2025-04-07 14:25:59 +0800

    misc: add error

--- app/base/error.py
+++ app/base/error.py
@@ -5,8 +5,11 @@ class Error(Enum):
     Unknown = "unknown error"
     FileNotFound = 'file not found'
     TaskNotFount = 'task not found'
+    MNotExist = 'dir not exist'
     NetworkError = 'network error'
     CredentialsError = 'credentials error'
     InvalidParameter = 'invalid parameter'
     UnsupportFile = 'unsupport file'
     FileSizeExceeded = 'file size exceeded'
+    NoFaceDetected = 'no face detected'
+    UnknownResponse = 'unknown response'
