e6bc83e3bd89055cac20257a986bb14017c4116c
er1cw00 <wadahana@qq.com>
2025-04-23 21:16:12 +0800

    test: testcase

--- app/service/deep/test.py
+++ app/service/deep/test.py
@@ -26,18 +26,18 @@ if __name__ == '__main__':
     current_path = os.getcwd() #os.path.dirname(os.path.abspath(__file__))
     #task_path = os.path.join(current_path, "test/ABCDEFGHIJKL")
     #os.mkdir(task_path, exist_ok=True)
-    task_path = "/Users/wadahana/workspace/AI/tbox.ai/data/deep/task/20250423/71cc479c0d8ef0c05cce86b24e0a9bf9"
+    task_path = "/Users/wadahana/workspace/AI/tbox.ai/data/deep/task/20250423/e6fb79adb157a4d9e28167b06e603752"
     task_info = {
         'uid': '1234567890',
-        'task_id': '71cc479c0d8ef0c05cce86b24e0a9bf9',
+        'task_id': 'e6fb79adb157a4d9e28167b06e603752',
         'task_type': TaskType.FaceSwap,
         'task_state': TaskState.InProgress,    
         'priority': 1,
         'credit': 1,
         'start_time': 1745104229,
         'update_time': 1745104300,
-        'format': 'jpg',
-        'video': False,
+        'format': 'mp4',
+        'video': True,
         'obj_keys': {} 
     }
     
